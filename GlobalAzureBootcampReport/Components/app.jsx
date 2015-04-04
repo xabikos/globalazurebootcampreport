2679de6de3ffb4969b61dceac76a04686828221a
Babis Karypidis <xabikos@hotmail.com>
2015-04-04 19:03:42 +0300

    Move the app core jsx component in separate file

---
+++ GlobalAzureBootcampReport/Components/app.jsx
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+class ExampleApplication extends React.Component {
+    render() {
+        var message = `React has been successfully running for ${this.props.elapsed} seconds.`;
+
+        return <p>
+                {message}
+            </p>;
+    }
+}        
+React.render(React.createElement(ExampleApplication, { elapsed: 100 }), document.getElementById('app'));
