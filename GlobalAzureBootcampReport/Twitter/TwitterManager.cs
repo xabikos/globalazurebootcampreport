f4b9709f60384f8d6472612bfa3bca44cc6792e5
Babis Karypidis <xabikos@hotmail.com>
2015-04-23 19:34:43 +0300

    Change the term to listen to

--- GlobalAzureBootcampReport/Twitter/TwitterManager.cs
+++ GlobalAzureBootcampReport/Twitter/TwitterManager.cs
@@ -49,7 +49,7 @@ namespace GlobalAzureBootcampReport.Twitter
             if (_stream == null)
             {
                 _stream = Stream.CreateFilteredStream();
-                _stream.AddTrack("#earthday");
+                _stream.AddTrack("#WorldBookDay");
 
                 _stream.MatchingTweetReceived += (sender, args) =>
                 {
